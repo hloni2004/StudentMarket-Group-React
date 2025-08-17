c832ee8897d7b0a029e37e2b0a4b30c43d335c9d
nkululeko <nkulufr98@gmail.com>
2025-08-17 17:31:21 +0200

    fix login

--- src/service/StudentService.js
+++ src/service/StudentService.js
@@ -4,18 +4,18 @@ const REST_API_BASE_URL = 'http://localhost:8080/api';
 
 //create a student when signing up: Sign up
 export const captureStudentDetails = (student) => {
-  return axios.post(REST_API_BASE_URL + '/students/create', student);
+  return axios.post(REST_API_BASE_URL + '/student/create', student);
   
 }
 
 //get all student for administrator: Student mangement
 export const listRegisteredStudents = () => {
-    return axios.get(REST_API_BASE_URL + '/students/getAllStudents');
+    return axios.get(REST_API_BASE_URL + '/student/getAllStudent');
 }
 
-// Login function - authenticate student
-export const authenticateStudent = (loginData) => {
-  return axios.get(REST_API_BASE_URL + '/students/login', loginData);
+// Login function - authenticate student/admin
+export const authenticateUser = (loginData) => {
+  return axios.get(REST_API_BASE_URL + '/student/login',{params: loginData});
 }
 
 
