4f93c3c8957dc2e7aaa9630b4549f9c1a7725a62
Lehlohonolo Mokoena <157991204+hloni2004@users.noreply.github.com>
2025-08-31 22:57:34 +0200

    Update StudentService.js

--- src/service/StudentService.js
+++ src/service/StudentService.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const REST_API_BASE_URL = 'http://localhost:8080/api';
 
-//create a student when signing up: Sign up
+
 export const captureStudentDetails = (student) => {
   return axios.post(REST_API_BASE_URL + '/student/create', student);
   
 }
 
-//get all student for administrator: Student mangement
+
 export const listRegisteredStudents = () => {
     return axios.get(REST_API_BASE_URL + '/student/getAll');
 }
 
-// Login function - authenticate student/admin
+
 export const authenticateUser = (loginData) => {
   return axios.get(REST_API_BASE_URL + '/student/login',{params: loginData});
 }
